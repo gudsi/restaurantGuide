500a675dd3c9e924d87e2940a1bf30a01da77f3e
Gudrun <Gudrun.huszar@gmx.at>
2018-11-06 21:08:37 +0100

    Request to OSM

--- app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
+++ app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
@@ -10,4 +10,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    <osm-script>
+  <query type="node">
+    <has-kv k="amenity" v="restaurant"/>
+<has-kv k="wheelchair" v="yes"/>
+<around radius="1000.0" lat="41.89248629819397" lon="12.51119613647461"/>
+  </query>
+  <print/>
+</osm-script>
+     */
 }
