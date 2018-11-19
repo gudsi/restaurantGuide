668b8ab71f0e140c38f09fc942ef4eeeeff7ab18
Gudrun <Gudrun.huszar@gmx.at>
2018-11-19 14:32:52 +0100

    GPS included

--- app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
+++ app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.gudrun.restaurantguide;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView lat;
+    TextView lon;
+
+   // @RequiresApi(api = Build.VERSION_CODES.M)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lat = (TextView) findViewById(R.id.textView2);
+        lon = (TextView) findViewById(R.id.textView4);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener ll = new myLocationListener();
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+    }
+
+
+    class myLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            // TODO Auto-generated method stub
+            if(location!=null){
+                double pLat=location.getLatitude();
+                double pLong=location.getLongitude();
+                lat.setText(Double.toString(pLat));
+                lon.setText(Double.toString(pLong));
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+
+        }
     }
 
     /*
@@ -22,3 +83,4 @@ public class MainActivity extends AppCompatActivity {
 </osm-script>
      */
 }
+//TODO - set the put request and store result in a file.
