d73f67bf706b29778155702163e3e0aed4ae05c6
Gudrun <Gudrun.huszar@gmx.at>
2018-11-22 09:24:50 +0100

    Compass included

--- app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
+++ app/src/main/java/com/example/gudrun/restaurantguide/MainActivity.java
@@ -3,20 +3,35 @@ package com.example.gudrun.restaurantguide;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     TextView lat;
     TextView lon;
 
-   // @RequiresApi(api = Build.VERSION_CODES.M)
+    //Compass
+    private SensorManager sensorManager;
+    private Sensor compass;
+    private ImageView image;
+    private TextView compassAngle;
+    private float currentDegree = 0f;
+
+    // @RequiresApi(api = Build.VERSION_CODES.M)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         LocationListener ll = new myLocationListener();
 
+        // allowing app to get location of phone
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    Activity#requestPermissions
@@ -38,6 +54,59 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+
+
+        //Compass
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        compassAngle = (TextView) findViewById(R.id.angle);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        compass = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (compass != null) {
+
+            sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_NORMAL);
+
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = Math.round(event.values[0]);
+        compassAngle.setText("Heading : " + Float.toString(degree) + " degrees");
+        RotateAnimation ra = new RotateAnimation(currentDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
 
