ae4ba6a18214ad4348bcfa0512d88e336ec604f3
Gudrun <Gudrun.huszar@gmx.at>
2019-01-27 11:55:52 +0100

    list selection of restaurant names

--- app/src/main/java/com/example/gudrun/restaurantguide/ListActivity.java
+++ app/src/main/java/com/example/gudrun/restaurantguide/ListActivity.java
@@ -34,18 +34,12 @@ public class ListActivity extends AppCompatActivity {
         String nodeListString = getIntent().getStringExtra("nodeList");
         System.out.println("nlString: " + nodeListString);
         String[] names =new String[10];
-        parsexml(getIntent().getStringExtra("nodeList")).toArray(names);
         lv=findViewById(R.id.listview);
+        parsexml(getIntent().getStringExtra("nodeList")).toArray(names);
+
        // ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
        // ArrayAdapter<Node> adapter=new ArrayAdapter<Node>(this,android.R.layout.simple_list_item_1,restaurants);
        // lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),"Item Clicked:"+i,Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
     private static String getValue(String tag, Element element) {
         NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
@@ -77,104 +71,17 @@ public class ListActivity extends AppCompatActivity {
                         tmp.add(eElement.getAttribute("v"));
                     }
                     System.out.println("value : " + eElement.getAttribute("v"));
-                }}
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                    this,
-                    android.R.layout.simple_list_item_1,
-                    tmp );
+                }
+            }
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tmp );
 
             lv.setAdapter(arrayAdapter);
-
-
-           /* Element element=doc.getDocumentElement();
-            element.normalize();
-
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-
-            receveidRestaurant = doc.getElementsByTagName("node");    // list of every restaurant
-
-            for(int i = 0; i < receveidRestaurant.getLength(); i++) {
-                Node rest = receveidRestaurant.item(i);
-                NodeList data = rest.getChildNodes();
-                for(int j = 0; j < data.getLength(); j++ ) {
-                    Node child = data.item(j);
-                    NodeList tags = child.getChildNodes();
-
-                    System.out.println("tags: " + tags);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Toast.makeText(getApplicationContext(),"Item Clicked:"+i,Toast.LENGTH_SHORT).show();
                 }
-            }
-
-            /*
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-
-                Node nNode = nList.item(temp);
-                System.out.println("node: " + nNode.getAttributes());
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    //System.out.println("node: " + nNode.getAttributes());
-                    Element eElement = (Element) nNode;
-                    NodeList nodeList = eElement.getElementsByTagName("name");
-                    System.out.println(nodeList.item(0).getNodeValue());
-                   // System.out.println("key : " + eElement.getAttribute("k"));
-                    //System.out.println("value : " + eElement.getAttribute("v"));
-                }}
-
-           /* Element element=doc.getDocumentElement();
-            element.normalize();
-
-            NodeList nList = doc.getElementsByTagName("node");
-
-            for (int i=0; i<nList.getLength(); i++) {
-
-                Node node = nList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element2 = (Element) node;
-                    System.out.println("\nRestaurant Names : " + getValue("name",element2)+"\n");
-                    // tv1.setText(tv1.getText()+"-----------------------");
-                }
-            }
-
-
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = dbFactory.newDocumentBuilder();
-            Document doc = builder.parse(new InputSource(new StringReader(response)));
-            doc.getDocumentElement().normalize();
-
-            Element element=doc.getDocumentElement();
-            element.normalize();
-
-            NodeList nList = doc.getElementsByTagName("node");
-
-            for (int i=0; i<nList.getLength(); i++) {
-
-                Node node = nList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element2 = (Element) node;
-                    NodeList children = element2.getChildNodes();
-                    System.out.println(element2.getElementsByTagName("name"));
-                    for (int j = 0; j < children.getLength(); j++) {
-                       Node child = children.item(j);
-                    }
-                  //  System.out.println("\nRestaurant Names : " + getValue("name", element2));
-                    // tv1.setText(tv1.getText()+"-----------------------");
-                }
-            }
-
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("tag");
-            receveidRestaurant = doc.getElementsByTagName("node");    // list of every restaurant
-
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-
-                Node nNode = nList.item(temp);
-                System.out.println("node: " + nNode.getAttributes());
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    //System.out.println("node: " + nNode.getAttributes());
-                    Element eElement = (Element) nNode;
-                    System.out.println("\nRestaurant Names : " + getValue("name", eElement));
-                    System.out.println("key : " + eElement.getAttribute("k"));
-                    System.out.println("value : " + eElement.getAttribute("v"));
-                }
-            }*/
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
